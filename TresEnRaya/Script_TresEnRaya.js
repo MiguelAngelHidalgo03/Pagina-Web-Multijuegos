d5ab93e79cb7d7c791e853e1aef51e8bc82510bc
MiguelAngelHidalgo03 <miguelahidalgo03@gmail.com>
2024-10-11 12:58:50 +0200

    Botón página principal 3.0

--- TresEnRaya/Script_TresEnRaya.js
+++ TresEnRaya/Script_TresEnRaya.js
@@ -120,17 +120,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const volverMenu = document.getElementById("volverMenu");
 
     volverMenu.addEventListener("click", function() {
-        const origin = window.location.origin;
-    
-        if (origin.includes("github.io")) {
-            window.location.href = "https://miguelangelhidalgo03.github.io/Pagina-Web-Multijuegos/";
-        } else {
-            window.location.href = "/index.html";
-        }
-    });
+
+    window.location.href = "https://miguelangelhidalgo03.github.io/Pagina-Web-Multijuegos/";
     
 });
 
+});
 
 //Boton MOSTRAR resultado victorias, derrotas, empates
 document.addEventListener("DOMContentLoaded", function() {
